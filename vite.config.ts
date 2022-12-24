094dbb19d4fdf31fba15152c9e55d66fbacfbef4
gavrushuk <havryschuk@gmail.com>
2022-12-24 20:24:08 +0200

    config and package updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue3-countdown',
+  base: '/vue3-countdown/',
   plugins: [vue()],
   resolve: {
     alias: {
